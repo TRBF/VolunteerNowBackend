9d62ce12a851c23c02d4511ad4ec54b6143ed8ff
devilExE3 <devilexe1337@gmail.com>
2024-09-15 19:39:38 +0300

    asdasddsa

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -15,23 +15,24 @@ export default function (app : Hono, db : Database)
         if(authorization == null) return c.json(fail("invalid request"));
         const userdata = await authorizeUser(db, authorization);
         if(userdata == null) return c.json(fail("invalid token"));
-        const body = await c.req.parseBody();
-        if(body.file == null) return c.json(fail("no file is present"));
-        console.log("new file: " + body.file);
-        if(!(body.file instanceof File)) return c.json(fail("file is not File type"));
-        const buffer = Buffer.from(await (body.file as File).arrayBuffer());
+        const body = await c.req.formData();
+        console.log(body);
+        // if(body.file == null) return c.json(fail("no file is present"));
+        // console.log("new file: " + body.file);
+        // if(!(body.file instanceof File)) return c.json(fail("file is not File type"));
+        // const buffer = Buffer.from(await (body.file as File).arrayBuffer());
         
-        // compute asset hash
-        const hash = crypto.createHash('sha256').update(buffer).digest('hex');
-        if(fs.existsSync(`${process.cwd()}/_stored_assets/${hash}`))
-            return c.json(success(hash)); // the file already exists, return the hash
-        // write file
-        var extension = (body.file as File).name.split('.').findLast(_ => true);
-        if(!fs.existsSync(`${process.cwd()}/_stored_assets`))
-            fs.mkdirSync(`${process.cwd()}/_stored_assets`);
-        fs.writeFileSync(`${process.cwd()}/_stored_assets/${hash}`, buffer);
+        // // compute asset hash
+        // const hash = crypto.createHash('sha256').update(buffer).digest('hex');
+        // if(fs.existsSync(`${process.cwd()}/_stored_assets/${hash}`))
+        //     return c.json(success(hash)); // the file already exists, return the hash
+        // // write file
+        // var extension = (body.file as File).name.split('.').findLast(_ => true);
+        // if(!fs.existsSync(`${process.cwd()}/_stored_assets`))
+        //     fs.mkdirSync(`${process.cwd()}/_stored_assets`);
+        // fs.writeFileSync(`${process.cwd()}/_stored_assets/${hash}`, buffer);
         
-        return c.json(success(hash));
+        // return c.json(success(hash));
     });
 
     app.get("/assets/:asset_id", async (c) => {
